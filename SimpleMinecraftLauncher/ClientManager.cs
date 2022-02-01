cefab325e6480ac6140ea6dd5104d0b379c275ef
Artem Black <artem.dart.32@gmail.com>
2022-02-01 13:10:20 +0300

    Test commit

--- SimpleMinecraftLauncher/ClientManager.cs
+++ SimpleMinecraftLauncher/ClientManager.cs
@@ -210,7 +210,10 @@ namespace SimpleMinecraftLauncher
                 Log("Удаление " + V.mVersionArchiveName + " прошло успешно");
             });
         }
-
+        /// <summary>
+        /// hui.
+        /// </summary>
+        /// <param name="V"></param>
         internal void InstallClient(Version V)
         {
             AsyncWorker.PollAsyncMethod(async () =>
